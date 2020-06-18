47163a6b6f6a0abf2ad6e48cb466a17063381820
daddyUrsa <0057747@gmail.com>
2020-06-18 13:42:41 +0300

    Finished beta PropertyAnimation

--- Course2Week4Task2/Code/Animations/UIViewPropertyAnimatorController.swift
+++ Course2Week4Task2/Code/Animations/UIViewPropertyAnimatorController.swift
@@ -8,45 +8,42 @@
 import UIKit
 
 class UIViewPropertyAnimatorController: UIViewController {
-    
-    var animator: UIViewPropertyAnimator!
+
     @IBOutlet weak var animationView: UIView!
-    
+
+    var animator = UIViewPropertyAnimator()
+
     @IBAction func panGestureHandler(recognizer: UIPanGestureRecognizer) {
         switch recognizer.state {
-        case .began:
-            animationView.backgroundColor = .orange
-            animator = UIViewPropertyAnimator(duration: 1.0, curve: .linear, animations: {
-                self.animationView.center.x += 300.0
-                self.animationView.transform = CGAffineTransform(scaleX: 3, y: 3)
+            case .began:
+                animator = UIViewPropertyAnimator(duration: 1.0, timingParameters: UISpringTimingParameters(mass: 2.0,
+                                                                                                            stiffness: 30.0,
+                                                                                                            damping: 7.0,
+                                                                                                            initialVelocity: .zero))
                 self.animationView.transform = CGAffineTransform(rotationAngle: .pi)
-            })
-            animator.addCompletion { _ in
-                self.animationView.backgroundColor = .red
-            }
-            animator.pauseAnimation()
-//            print("Began")
-        case .changed:
-            animator.fractionComplete = recognizer.translation(in: view).x / view.bounds.size.width
+                animator.addAnimations {
+                    self.animationView.layer.position.x += 300.0
+                    self.animationView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+                }
+
             animator.pauseAnimation()
 
-//            print("Changed")
-        case .ended:
-            print("animationView x = \(animationView.layer.position.x - 300), у = \(animationView.layer.position.y), View x = \(view.center.x))")
-            if (animationView.layer.position.x - 300) < view.center.x {
-                animator.isReversed = true
+            case .changed:
+                animator.fractionComplete = recognizer.translation(in: view).x / view.bounds.size.width
+
+            case .ended:
+                if animationView.layer.position.x < view.center.x {
+                    animator.isReversed = false
+                } else {
+                    animator.isReversed = true
+                }
                 animator.startAnimation()
-                print("Меньше")
-            } else {
-                animator.stopAnimation(true)
-                print("Больше")
-            }
 
-            print(Double(animationView.center.x - 300) < Double(view.center.x))
-//            print("End")
-        default:
-            ()
+            default:
+                ()
         }
-        
+
     }
 }
+
+
